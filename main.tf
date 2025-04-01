ff7032de6032a7efd9b88b13ac2e3a38899a4d70
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-04-01 15:16:28 +1000

    adding task definition resource

--- main.tf
+++ main.tf
@@ -36,6 +36,48 @@ data "aws_subnets" "private" {
 resource "aws_ecs_cluster" "taskoverflow" { 
    name = "taskoverflow" 
 }
+
+resource "aws_ecs_task_definition" "taskoverflow" { 
+   family = "taskoverflow" 
+   network_mode = "awsvpc" 
+   requires_compatibilities = ["FARGATE"] 
+   cpu = 1024 
+   memory = 2048 
+   execution_role_arn = data.aws_iam_role.lab.arn 
+ 
+   container_definitions = <<DEFINITION
+ [ 
+   { 
+    "image": "${local.image}", 
+    "cpu": 1024, 
+    "memory": 2048, 
+    "name": "todo", 
+    "networkMode": "awsvpc", 
+    "portMappings": [ 
+      { 
+       "containerPort": 6400, 
+       "hostPort": 6400 
+      } 
+    ], 
+    "environment": [ 
+      { 
+       "name": "SQLALCHEMY_DATABASE_URI", 
+       "value": "postgresql://${local.database_username}:${local.database_password}@${aws_db_instance.taskoverflow_database.address}:${aws_db_instance.taskoverflow_database.port}/${aws_db_instance.taskoverflow_database.db_name}" 
+      } 
+    ], 
+    "logConfiguration": { 
+      "logDriver": "awslogs", 
+      "options": { 
+       "awslogs-group": "/taskoverflow/todo", 
+       "awslogs-region": "us-east-1", 
+       "awslogs-stream-prefix": "ecs", 
+       "awslogs-create-group": "true" 
+      } 
+    } 
+   } 
+ ] 
+ DEFINITION 
+}
  
 resource "aws_db_instance" "taskoverflow_database" { 
     allocated_storage = 20 
