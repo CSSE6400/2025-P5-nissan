33227119a76abbcdb7fa5e5b91c04aa9769d05e5
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-04-01 15:01:12 +1000

    do a manual database creation based on lab text

--- main.tf
+++ main.tf
@@ -12,79 +12,51 @@ provider "aws" {
    shared_credentials_files = ["./credentials"] 
 }
 
-# Get the default VPC
-data "aws_vpc" "default" {
-  default = true
+locals { 
+    database_username = "administrator" 
+    database_password = "foobarbaz" # This is bad! 
+} 
+ 
+resource "aws_db_instance" "taskoverflow_database" { 
+    allocated_storage = 20 
+    max_allocated_storage = 1000 
+    engine = "postgres" 
+    engine_version = "17" 
+    instance_class = "db.t3.micro" 
+    db_name = "todo" 
+    username = local.database_username 
+    password = local.database_password 
+    parameter_group_name = "default.postgres17" 
+    skip_final_snapshot = true 
+    vpc_security_group_ids = [aws_security_group.taskoverflow_database.id] 
+    publicly_accessible = true 
+ 
+    tags = { 
+        Name = "taskoverflow_database" 
+    } 
 }
 
-# Get all subnets in the default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-
-  filter {
-    name   = "default-for-az"
-    values = ["true"]
-  }
+resource "aws_security_group" "taskoverflow_database" { 
+    name = "taskoverflow_database" 
+    description = "Allow inbound Postgresql traffic" 
+ 
+    ingress { 
+        from_port = 5432 
+        to_port = 5432 
+        protocol = "tcp" 
+        cidr_blocks = ["0.0.0.0/0"] 
+    } 
+ 
+    egress { 
+        from_port = 0 
+        to_port = 0 
+        protocol = "-1" 
+        cidr_blocks = ["0.0.0.0/0"] 
+        ipv6_cidr_blocks = ["::/0"] 
+    } 
+ 
+    tags = { 
+        Name = "taskoverflow_database" 
+    } 
 }
 
-# Security group for RDS
-resource "aws_security_group" "todogroup" {
-  name        = "todogroup"
-  description = "Security group for todo RDS instance"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "todogroup"
-  }
-}
-
-# RDS instance
-resource "aws_db_instance" "todo" {
-  identifier           = "todo"
-  engine              = "postgres"
-  engine_version      = "17"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 20
-  storage_type        = "gp2"
-  
-  db_name             = "todo"
-  username           = "todoadmin"
-  password           = "Todoadmin123$"
-  
-  skip_final_snapshot = true
-  publicly_accessible = true
-  
-  vpc_security_group_ids = [aws_security_group.todogroup.id]
-  db_subnet_group_name   = aws_db_subnet_group.todo.name
-
-  tags = {
-    Name = "todo"
-  }
-}
-
-# DB subnet group
-resource "aws_db_subnet_group" "todo" {
-  name       = "todo-subnet-group"
-  subnet_ids = data.aws_subnets.default.ids
-
-  tags = {
-    Name = "todo-subnet-group"
-  }
-}
