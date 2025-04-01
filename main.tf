e71321d0e1a6783eea16df685199a283d82672f6
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-04-01 15:21:52 +1000

    add ecs configuration, success deployment

--- main.tf
+++ main.tf
@@ -78,7 +78,46 @@ resource "aws_ecs_task_definition" "taskoverflow" {
  ] 
  DEFINITION 
 }
+
+resource "aws_ecs_service" "taskoverflow" { 
+   name = "taskoverflow" 
+   cluster = aws_ecs_cluster.taskoverflow.id 
+   task_definition = aws_ecs_task_definition.taskoverflow.arn 
+   desired_count = 1 
+   launch_type = "FARGATE" 
  
+   network_configuration { 
+    subnets = data.aws_subnets.private.ids 
+    security_groups = [aws_security_group.taskoverflow.id] 
+    assign_public_ip = true 
+   } 
+}
+resource "aws_security_group" "taskoverflow" { 
+   name = "taskoverflow" 
+   description = "TaskOverflow Security Group" 
+ 
+   ingress { 
+    from_port = 6400 
+    to_port = 6400 
+    protocol = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+   } 
+ 
+   ingress { 
+    from_port = 22 
+    to_port = 22 
+    protocol = "tcp" 
+    cidr_blocks = ["0.0.0.0/0"] 
+   } 
+ 
+   egress { 
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+   } 
+}
+
 resource "aws_db_instance" "taskoverflow_database" { 
     allocated_storage = 20 
     max_allocated_storage = 1000 
