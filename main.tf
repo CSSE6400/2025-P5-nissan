125c8a92b1bc2d829709d55f81a6eadad00d4e40
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-04-01 15:15:36 +1000

    added text works for terraform plan to here

--- main.tf
+++ main.tf
@@ -13,9 +13,29 @@ provider "aws" {
 }
 
 locals { 
+    image = "ghcr.io/csse6400/taskoverflow:latest" 
     database_username = "administrator" 
     database_password = "foobarbaz" # This is bad! 
+}
+
+data "aws_iam_role" "lab" { 
+   name = "LabRole" 
 } 
+ 
+data "aws_vpc" "default" { 
+   default = true 
+} 
+ 
+data "aws_subnets" "private" { 
+   filter { 
+      name = "vpc-id" 
+      values = [data.aws_vpc.default.id] 
+   } 
+}
+
+resource "aws_ecs_cluster" "taskoverflow" { 
+   name = "taskoverflow" 
+}
  
 resource "aws_db_instance" "taskoverflow_database" { 
     allocated_storage = 20 
